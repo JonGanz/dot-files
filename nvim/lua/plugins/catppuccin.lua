b7f7ac025c0773d567cb6ef610acdc7d8bc5428d
Jon Ganz <jon.ganz@activate.ca>
2024-01-17 22:26:57 -0600

    disable italics; change grep shortcut

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,11 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+        require('catppuccin').setup({
+            flavour = 'mocha',
+            transparent_background = true,
+            no_italic = true,
+        })
         vim.cmd.colorscheme 'catppuccin-mocha'
     end
 }
