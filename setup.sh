64ad149242260cbae8d2b4d19aa6370569425212
JonGanz <jonganz.dev@gmail.com>
2024-04-29 21:49:47 -0500

    added a setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+mkdir -p ~/setup
+git clone https://github.com/JonGanz/dot-files ~/setup
+
