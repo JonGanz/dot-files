be34e4135c8d26595a2bcf7322167c83cb08f1ef
Jon Ganz <jonganz.dev@gmail.com>
2025-02-11 00:07:43 -0600

    added .gitconfig setup; fixed SSH config; todo notes

--- setup.sh
+++ setup.sh
@@ -4,4 +4,11 @@ sudo apt install git software-properties-common
 # Using install from Ubuntu Universe since ansible hasn't published for Noble 24.04 yet.
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+
+# TODO: Checkout the repository at ~/setup
+# TODO: Prompt for the Vault password, dump in vault.cred
+
 ansible-playbook wsl.yml -K
+
+# TODO: Update the Repo URL (which assumes the playbook correctly installed the SSH keys)
+
