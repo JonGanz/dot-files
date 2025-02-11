6d18b056b901a7fb5a5dd0b38d515bee9d9d8715
Jon Ganz <jonganz.dev@gmail.com>
2025-02-11 11:07:07 -0600

    updated setup script to clone the repo and swap origin from HTTPS to SSH

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+
 sudo apt update
 sudo apt install git software-properties-common
+
 # Using install from Ubuntu Universe since ansible hasn't published for Noble 24.04 yet.
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
-# TODO: Checkout the repository at ~/setup
+mkdir -p ~/setup
+git clone https://github.com/JonGanz/dot-files ~/setup
+cd ~/setup
+git checkout feature/windows-setup
+
 # TODO: Prompt for the Vault password, dump in vault.cred
 
-ansible-playbook wsl.yml -K
+# Commenting out the auto-run for now since we haven't pulled in the vault credentials yet.
+#ansible-playbook wsl.yml -K
 
-# TODO: Update the Repo URL (which assumes the playbook correctly installed the SSH keys)
+git remote set-url origin git@github.com:JonGanz/dot-files.git
 
