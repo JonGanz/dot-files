3e72b849e34d1118e2ebdcec934bfddc4b4bc167
Jon Ganz <jonganz.dev@gmail.com>
2025-02-17 19:20:01 -0600

    nvim: <C-s> brings up Telescope document symbols

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,7 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<C-p>', builtin.find_files, {})
             vim.keymap.set('n', '<C-F>', builtin.live_grep, {})
+            vim.keymap.set('n', '<C-s>', builtin.lsp_document_symbols, {})
         end
     },
     {
