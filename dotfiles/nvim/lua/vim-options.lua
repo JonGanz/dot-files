38512ac2b94e1e3cb74a245dad793fc96ba690f9
Jon Ganz <jonganz.dev@gmail.com>
2025-02-21 17:43:33 -0600

    nvim: fix for something breaking quickfix file selection

--- dotfiles/nvim/lua/vim-options.lua
+++ dotfiles/nvim/lua/vim-options.lua
@@ -1,21 +1,26 @@
+vim.g.mapleader = ' '
+
+vim.cmd('set colorcolumn=90')
 vim.cmd('set expandtab')
-vim.cmd('set tabstop=4')
-vim.cmd('set softtabstop=4')
-vim.cmd('set shiftwidth=4')
 vim.cmd('set number')
 vim.cmd('set relativenumber')
-vim.cmd('set colorcolumn=90')
-vim.g.mapleader = ' '
+vim.cmd('set shiftwidth=4')
+vim.cmd('set softtabstop=4')
+vim.cmd('set tabstop=4')
 
 -- Keep things centered vertically.
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 
--- paste over something highlighted without losing the thing you yanked
+-- Paste over something highlighted without losing the thing you yanked.
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- yank to clipboard?
+-- Yank to clipboard.
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- Something is preventing <CR> from opening the file in the quickfix list.
+-- This prevents that from happening.
+vim.cmd('autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>')
 
