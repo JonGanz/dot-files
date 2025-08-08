7d973de7b9bc8aacbb3a143ff3323bc9a0279b5a
Jon Ganz <jonganz.dev@gmail.com>
2025-08-08 11:34:58 -0500

    nvim: added null_ls prettier formatting support for web files/markdown

--- dotfiles/nvim/lua/plugins/none-ls.lua
+++ dotfiles/nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,9 @@ return {
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettier.with({
+                        filetypes = { 'html', 'css', 'json', 'markdown', 'scss' },
+                    }),
                 },
             })
 
