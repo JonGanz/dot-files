fe23dcb0c3b9dc793c1edd93f8f90e4333abfb6b
Jon Ganz <jonganz.dev@gmail.com>
2025-10-08 22:11:55 +0000

    nvim: added text-case

--- dotfiles/nvim/lua/plugins/editor.lua
+++ dotfiles/nvim/lua/plugins/editor.lua
@@ -1,5 +1,30 @@
 return {
     {
+        'johmsalas/text-case.nvim',
+        dependencies = {
+            'nvim-telescope/telescope.nvim', -- ui.lua
+        },
+        config = function()
+            require('textcase').setup({
+                prefix = '<leader>w',
+                enabled_methods = {
+                    'to_camel_case', -- c
+                    'to_constant_case', -- n
+                    'to_dash_case', -- d
+                    'to_lower_case', -- l
+                    'to_pascal_case', -- p
+                    'to_snake_case', -- s
+                    'to_upper_case', -- u
+                },
+            })
+            require('telescope').load_extension('textcase')
+        end,
+        keys = {
+            '<leader>w',
+            { '<leader>w.', '<Cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = "Change case" },
+        },
+    },
+    {
         'nvim-mini/mini.comment',
         version = '*',
         event = { 'BufReadPre', 'BufNewFile' },
