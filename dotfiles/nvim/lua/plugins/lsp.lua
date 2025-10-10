907f0b9070eb968649f9d2fa31b432195984882c
Jon Ganz <jonganz.dev@gmail.com>
2025-10-10 15:33:03 +0000

    nvim: addressed required updates for Vue LSP

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -25,20 +25,6 @@ return {
     },
     {
         'mason-org/mason-lspconfig.nvim',
-        opts = {
-            ensure_installed = {
-                'ansiblels',
-                'clangd',
-                'eslint',
-                'gopls',
-                'lua_ls',
-                'marksman',
-                'omnisharp',
-                'sqlls',
-                'ts_ls',
-                'yamlls',
-            },
-        },
         dependencies = {
             {
                 'mason-org/mason.nvim',
@@ -54,6 +40,49 @@ return {
                 end,
             },
         },
+        opts = {
+            ensure_installed = {
+                'ansiblels',
+                'clangd',
+                'eslint',
+                'gopls',
+                'lua_ls',
+                'marksman',
+                'omnisharp',
+                'sqlls',
+                'ts_ls',
+                'vue_ls',
+                'yamlls',
+            },
+        },
+        config = function(opts)
+            require('mason-lspconfig').setup(opts)
+
+            -- Setup the Vue language support.
+            local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+            local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+            local vue_plugin = {
+                name = '@vue/typescript-plugin',
+                location = vue_language_server_path,
+                languages = { 'vue' },
+                configNamespace = 'typescript',
+            }
+
+            local ts_ls_config = {
+                init_options = {
+                    plugins = {
+                        vue_plugin,
+                    },
+                },
+                filetypes = tsserver_filetypes,
+            }
+
+            local vue_ls_config = {}
+
+            vim.lsp.config('vue_ls', vue_ls_config)
+            vim.lsp.config('ts_ls', ts_ls_config)
+            vim.lsp.enable({'ts_ls', 'vue_ls'})
+        end,
     },
     {
         'nvimtools/none-ls.nvim',
