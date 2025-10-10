60120f9da2905948939b6609c0a6e9b27a7bb7de
Jon Ganz <jonganz.dev@gmail.com>
2025-10-10 15:08:25 -0500

    theme: wip - moving from bamboo to gruvbox

--- dotfiles/nvim/lua/plugins/themes.lua
+++ dotfiles/nvim/lua/plugins/themes.lua
@@ -1,14 +1,14 @@
 return {
-	'ribru17/bamboo.nvim',
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require('bamboo').setup {
-            lualine = {
-                transparent = true,
-            },
-            transparent = true,
-		}
-		require('bamboo').load()
-	end,
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_background = 'hard'
+            vim.g.gruvbox_material_transparent_background = 2
+            vim.g.gruvbox_material_enable_bold = 1
+            vim.g.gruvbox_material_ui_contrast = 'high'
+            vim.cmd.colorscheme 'gruvbox-material'
+        end,
+    },
 }
