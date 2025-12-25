6e54877bf70dc396695e647fe10d2b7d60c3676f
Jon Ganz <jonganz.dev@gmail.com>
2025-12-24 20:41:22 -0600

    bash script: migrated nodejs

---
+++ scripts/installs/nodejs.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${NVM_VERSION:?NVM_VERSION not set}"
+: "${SETUP_DIR:?SETUP_DIR not set}"
+
+source "$SETUP_DIR/bash/ensure.sh"
+
+if [[ $OS == "ubuntu" ]]; then
+
+    if ! command -v nvm >/dev/null 2>&1; then
+        curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | bash
+    fi
+
+    NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+    CURR_NVM_VERSION=$(nvm --version)
+
+    if [[ "$CURR_NVM_VERSION" != "$NVM_VERSION" ]]; then
+        curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | bash
+    fi
+
+elif [[ $OS == "arch" ]]; then
+
+    sudo pacman -S --noconfirm --needed \
+        nvm
+
+    source /usr/share/nvm/init-nvm.sh
+fi
+
+
+    ensure_block_in_file "$HOME/.bashrc" "NVM Bash Autocomplete" "$(cat <<EOF
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
+    )"
+
+
+if ! command -v node >/dev/null 2>&1; then
+    nvm install --lts=krypton
+    nvm use --lts
+fi
+
