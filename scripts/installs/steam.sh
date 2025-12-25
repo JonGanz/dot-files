65e1138a1ffcd9d71c7ba71d61d7db17d9608c49
Jon Ganz <jonganz.dev@gmail.com>
2025-12-24 20:27:06 -0600

    bash setup: added steam for ubuntu

---
+++ scripts/installs/steam.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $OS == "ubuntu" ]]; then
+
+    if ! command -v steam >/dev/null 2>&1; then
+        local DEB_FILE_PATH="/tmp/steam.deb"
+        curl -fsSL "https://cdn.fastly.steamstatic.com/client/installer/steam.deb" -o "$DEB_FILE_PATH"
+        sudo dpkg -i "$DEB_FILE_PATH"
+    fi
+
+fi
+
