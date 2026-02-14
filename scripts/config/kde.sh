35afe16dfb214a869b501b9bf117ba909788c7e6
JonGanz <jonganz.dev@gmail.com>
2026-02-14 13:46:06 -0600

    kde: added what I expect are the necessary KDE config files

---
+++ scripts/config/kde.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+source "$SETUP_DIR/bash/ensure.sh"
+
+ensure_symlink_file "$SETUP_DIR/dotfiles/kde/kdeglobals" "$HOME/.config/kdeglobals"
+ensure_symlink_file "$SETUP_DIR/dotfiles/kde/kwinrc" "$HOME/.config/kwinrc"
+ensure_symlink_file "$SETUP_DIR/dotfiles/kde/plasma-org.kde.plasma.desktop-appletsrc" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
+ensure_symlink_file "$SETUP_DIR/dotfiles/kde/plasmarc" "$HOME/.config/plasmarc"
+
