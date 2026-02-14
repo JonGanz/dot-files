424def377545370c89ffb47f024fe718d95b1184
JonGanz <jonganz.dev@gmail.com>
2026-02-14 14:30:55 -0600

    kde: added global shortcut config file

--- scripts/config/kde.sh
+++ scripts/config/kde.sh
@@ -6,6 +6,7 @@ source "$SETUP_DIR/bash/ensure.sh"
 ensure_symlink_dir "$SETUP_DIR/wallpapers/gruvbox/dark" "/usr/local/share/wallpapers"
 
 ensure_symlink_file "$SETUP_DIR/dotfiles/kde/kdeglobals" "$HOME/.config/kdeglobals"
+ensure_symlink_file "$SETUP_DIR/dotfiles/kde/kglobalshortcutsrc" "$HOME/.config/kglobalshortcutsrc"
 ensure_symlink_file "$SETUP_DIR/dotfiles/kde/kwinrc" "$HOME/.config/kwinrc"
 ensure_symlink_file "$SETUP_DIR/dotfiles/kde/plasma-org.kde.plasma.desktop-appletsrc" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
 ensure_symlink_file "$SETUP_DIR/dotfiles/kde/plasmarc" "$HOME/.config/plasmarc"
