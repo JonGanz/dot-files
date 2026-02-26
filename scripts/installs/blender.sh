ea2dc36a07794af650335c04d404a0ee8cc8bbe6
JonGanz <jonganz.dev@gmail.com>
2026-02-25 23:12:25 -0600

    fixed Blender install script
    - changed from snap to apt-get for Ubuntu-like systems
    - added arch via pacman
    - avoiding install on system without DE

--- scripts/installs/blender.sh
+++ scripts/installs/blender.sh
@@ -2,9 +2,22 @@
 set -euo pipefail
 source "$SETUP_DIR/scripts/distro.fn.sh"
 
+if [[ "${HAS_DE:-0}" == "0" ]]; then
+    echo "Skipping Blender install for environment without Desktop Environment"
+    exit 2;
+fi
+
 if is_distro ubuntu; then
 
-    sudo snap install blender --classic
+    if command -v blender >/dev/null 2>&1; then
+        exit 0
+    fi
+
+    sudo apt-get install -y blender
+
+elif is_distro arch; then
+
+    sudo pacman -S --noconfirm --needed blender
 
 fi
 
