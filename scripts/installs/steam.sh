c5343ef7ba7182fede60278f1c5215c0e241217e
JonGanz <jonganz.dev@gmail.com>
2026-02-25 23:14:14 -0600

    added Steam dependencies for Ubuntu-like install

--- scripts/installs/steam.sh
+++ scripts/installs/steam.sh
@@ -9,6 +9,10 @@ if is_distro pop; then
 elif is_distro ubuntu; then
 
     if ! command -v steam >/dev/null 2>&1; then
+        sudo apt-get install -y \
+            zenity \
+            zenity-common \
+
         DEB_FILE_PATH="/tmp/steam.deb"
         curl -fsSL "https://cdn.fastly.steamstatic.com/client/installer/steam.deb" -o "$DEB_FILE_PATH"
         sudo dpkg -i "$DEB_FILE_PATH"
